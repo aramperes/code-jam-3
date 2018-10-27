a027ea43a0cd760e123a53509497b8db81156452
momothereal <momothereal.mc@gmail.com>
2018-10-27 02:08:20 -0400

    Push the client-host terrain debug stuff

--- services/client-host/project/static/terrain.js
+++ services/client-host/project/static/terrain.js
@@ -14,26 +14,39 @@ function colorFor(terrainTile) {
             return "#e5d85d";
         case "W":
             return "#4c7999";
+        case "fC": // city
+            return "#cc00b7";
+        case "fS": // shelter
+            return "#7d7d7d";
     }
     return "#000";
 }
 
 // load terrain
 $.getJSON("/static/terrain.json", (data) => {
+    const tileSize = 25;
     const terrain = data["terrain"];
-    const mapWidth = terrain.shift();
-    const mapHeight = terrain.shift();
+    const features = data["features"];
+    const mapWidth = data["width"];
+    const mapHeight = data["height"];
+
+    canvasElem.setAttribute("width", String(mapWidth * tileSize));
+    canvasElem.setAttribute("height", String(mapHeight * tileSize));
 
     for (let x = 0; x < mapWidth; x++) {
         for (let y = 0; y < mapHeight; y++) {
             const index = x * mapWidth + y;
-            if (x === 50 && y === 50) {
+            if (x === Math.floor(mapWidth / 2) && Math.floor(y === mapHeight / 2)) {
                 context.fillStyle = "#dd0200";
             } else {
                 context.fillStyle = colorFor(terrain[index]);
             }
-            context.fillRect(x * 10, y * 10, 10, 10);
+            context.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
+            if (!!features[index]) {
+                context.strokeStyle = colorFor(features[index]);
+                context.lineWidth = 3;
+                context.strokeRect(x * tileSize, y * tileSize, tileSize, tileSize);
+            }
         }
     }
-    console.log(terrain);
 });
